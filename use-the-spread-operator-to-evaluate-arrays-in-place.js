60bb2097d37b0e56aebb4daac600291141a92f4e
afroprogrammer <forsonfranklin@gmail.com>
2020-05-30 22:30:27 +0000

    Use the Spread Operator to Evaluate Arrays In-Place challenge completed

---
+++ use-the-spread-operator-to-evaluate-arrays-in-place.js
@@ -0,0 +1,13 @@
+//Copy all contents of arr1 into another array arr2 using the spread operator.
+
+/**HINT** 
+arr2 should be correct copy of arr1.
+... spread operator should be used to duplicate arr1.
+arr2 should remain unchanged when arr1 is changed.
+*/
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Change this line
+
+console.log(arr2);
